1f561d95bd22ae550a852e35d0110dcd3b108ba6
Alanthomas-2209 <2209@tkmce.ac.in>
2023-10-18 04:47:28 +0530

    facebook completed

--- CO2/FacebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity.java
+++ CO2/FacebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity.java
@@ -6,28 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button Login;
-
+    private Button loginButton;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Login = (Button) findViewById(R.id.login_button);
-        Login.setOnClickListener((new View.OnClickListener() {
+        loginButton = findViewById(R.id.login_button);
+        usernameEditText = findViewById(R.id.username_edittext);
+        passwordEditText = findViewById(R.id.password_edittext);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity2();
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (password.equals("password")) {
+                    openNextActivity(username);
+                } else {
+                    Toast.makeText(MainActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+                }
             }
-        }));
+        });
     }
 
-    private void openActivity2() {
+    private void openNextActivity(String username) {
         Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        //key value pair
+        intent.putExtra("username", username);
         startActivity(intent);
     }
 }
--- CO2/FacebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity2.java
+++ CO2/FacebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity2.java
@@ -2,13 +2,26 @@ package com.example.facebooklogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    private TextView usernameTextView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        usernameTextView = findViewById(R.id.username_textview);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            //key value pair
+            String username = extras.getString("username");
+            usernameTextView.setText(username);
+        }
     }
 }
