03a860dd3e781da46325b2834907bbc002131a03
Alanthomas-in <alan.thomas.in@gmail.com>
2024-01-07 23:02:02 +0530

    optionsMenu

--- CO3/OptionsMenu/app/src/main/java/com/example/optionsmenu/MainActivity.java
+++ CO3/OptionsMenu/app/src/main/java/com/example/optionsmenu/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
@@ -19,76 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main,menu);
         return true;
     }
 
-    @SuppressLint("NonConstantResourceId")
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if (itemId == R.id.action_item1) {
-            navigateToItem1();
-//            startActivity(new Intent(this, MainActivity2.class));
-            return true;
-        } else if (itemId == R.id.action_item2) {
-            navigateToItem2();
-//            startActivity(new Intent(this, MainActivity3.class));
-            return true;
-        } else {
-            return super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.itemHome) {
+            Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+            startActivity( new Intent(MainActivity.this, MainActivity2.class));
         }
+        else if(item.getItemId() == R.id.itemProfile) {
+            Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+            startActivity( new Intent(MainActivity.this, MainActivity3.class));
+        }
+        else if(item.getItemId() == R.id.itemAbout) {
+            Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+            startActivity( new Intent(MainActivity.this, MainActivity1.class));
+        }
+        else if(item.getItemId() == R.id.itemExit) {
+            Toast.makeText(this, "Exiting", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
-
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.action_item1:
-//                navigateToItem1();
-//                return true;
-//            case R.id.action_item2:
-//                navigateToItem2();
-//                return true;
-//            case R.id.action_item3:
-//                navigateToItem3();
-//                return true;
-//            case R.id.action_item4:
-//                navigateToItem4();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
-
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    private void navigateToItem1() {
-        showToast("Navigating to Item 1");
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
-    }
-
-    private void navigateToItem2() {
-        showToast("Navigating to Item 2");
-        Intent intent = new Intent(this, MainActivity3.class);
-        startActivity(intent);
-    }
-
-//    private void navigateToItem3() {
-//        showToast("Navigating to Item 3");
-//        Intent intent = new Intent(this, Item3Activity.class);
-//        startActivity(intent);
-//    }
-//
-//    private void navigateToItem4() {
-//        showToast("Navigating to Item 4");
-//        Intent intent = new Intent(this, Item4Activity.class);
-//        startActivity(intent);
-//    }
 }
