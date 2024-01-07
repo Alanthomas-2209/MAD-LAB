a7f41d613356b0ec84dcd40c161631b5902b5f1c
Alanthomas-in <alan.thomas.in@gmail.com>
2024-01-07 21:17:29 +0530

    recovery

--- CO3/implicitIntents/app/build.gradle.kts
+++ CO3/implicitIntents/app/build.gradle.kts
@@ -19,7 +19,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -31,7 +34,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- CO3/implicitIntents/settings.gradle.kts
+++ CO3/implicitIntents/settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ImplicitIntents"
+rootProject.name = "implicitIntents"
 include(":app")
  
